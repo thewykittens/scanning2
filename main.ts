c138a7d863a5566d526c1f8b9e253eed54c2ecb0
Jonas Katz <143116502+thewykittens@users.noreply.github.com>
2026-06-13 09:43:34 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+function Completion_accuracy (TRUEFALSE: boolean) {
+	
+}
+function Identify (myText: string) {
+	
+}
+function Task_completion_status_10 (_0: number) {
+	
+}
+function Search_at_1_for_2 (myLocation1: tiles.Location, mySprite2: Sprite) {
+	
+}
+function List_items (array: any[], item1: string, item2: string, item3: string, item4: string) {
+	
+}
+function Search (myLocation: tiles.Location) {
+	
+}
+function Scan_to_find (myImage: Image) {
+	
+}
+function Scan_for (mySprite: Sprite) {
+	
+}
